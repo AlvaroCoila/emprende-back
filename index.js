0ce686a1da57068b0f82e49d5e793e8e6537c8ba
Coila Gironda <ACoila@soboce.com>
2023-06-22 18:12:34 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { log } = require("console")
+//Declarar una variable con la palabra const
+ 
+const http = require("http")
+//Declarar una variable con la palabra const
+// "requiere" es un función que solo existe en node JS - nativo 
+// "import"  es una función nueva de JS
+const exportFromAnother = require('./another')
+console.log({exportFromAnother})
+
+const { CLIENT_RENEG_LIMIT } = require("tls")
+
+function requestController ()
+{
+// logica de nuestra funcion 
+console.log("Hola Mundo")
+}
+//configurar nuestro servidor
+const server = http.createServer(requestController)
+
+server.listen(5000)
