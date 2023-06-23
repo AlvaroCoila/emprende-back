3534d581dd5034817c0eb125bda26f24b133e016
Coila Gironda <ACoila@soboce.com>
2023-06-22 22:54:10 -0400

    quitamos another

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const http = require("http")
 //Declarar una variable con la palabra const
 // "requiere" es un función que solo existe en node JS - nativo 
 // "import"  es una función nueva de JS
-const exportFromAnother = require('./another')
-console.log({exportFromAnother})
+//const exportFromAnother = require('./another')
+//console.log({exportFromAnother})
 
 const { CLIENT_RENEG_LIMIT } = require("tls")
 const { doesNotThrow } = require("assert")
